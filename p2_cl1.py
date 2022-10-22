7abab9a02d9504435c42d0cef4b95a267a621316
ArtemScherbakov <artemscherbakov1422@gmail.com>
2022-10-22 15:05:04 +0300

    pr3

--- p2_cl1.py
+++ p2_cl1.py
@@ -1,8 +1,11 @@
 class Student:
     print("Hi!")
-    def __init__(self):
-        self.height = 160
+    def __init__(self, height=160):
+        self.height = height
         print('I am alive!')
 
-first_ek = Student
-print(first_ek.height)
+vasya = Student()
+print(vasya.height)
+
+dima = Student(height=186)
+print(dima.height)
